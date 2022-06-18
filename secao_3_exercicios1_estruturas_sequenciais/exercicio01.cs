528a8618ba6bdc32001ac1de169b7d3e80fd8a72
Cássio Freitas <cgf04df@outlook.com>
2022-06-18 19:26:55 -0300

    Cõdigo removido em função de falhas; refatorar.

---
+++ secao_3_exercicios1_estruturas_sequenciais/exercicio01.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secao_3_exercicios1_estruturas_sequenciais
+{
+    internal class Class1
+    {
+    }
+}
---
+++ secao_3_exercicios1_estruturas_sequenciais/principal.cs
@@ -0,0 +1,10 @@
+﻿namespace secao_3_exercicios1_estruturas_sequenciais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ");
+        }
+    }
+}
