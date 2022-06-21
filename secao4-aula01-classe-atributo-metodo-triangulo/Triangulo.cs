e8337ef35b6fc013874b9dd99b295baef90bd8b7
Cássio Freitas <cgf04df@outlook.com>
2022-06-20 21:17:33 -0300

    Exercitando o problema discutido na aula

---
+++ secao4-aula01-classe-atributo-metodo-triangulo/Triangulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secao4_aula01_classe_atributo_metodo_triangulo
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
